5b93599f6f9c99255b22b83d171603edd751734a
Srilekha mallela <srilekhamallela782@gmail.com>
2026-03-05 16:17:53 +0530

    Add files via upload

---
+++ Mood-Based Music Recommendation System.py
@@ -0,0 +1,222 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+= RESTART: C:/Users/admin/Desktop/srilekha/Mood-Based Music Recommendation System.py
+Current folder: C:\Users\admin\Desktop\srilekha
+Files in folder: ['adding digits methos 2.py', 'addition of numbers using type conversion logic.py', 'assignment 3 input function.py', 'assignment.py', 'assignment2.py', 'bill code using random.py', 'binary search.py', 'bitwise operations.py', 'BMI EXERCISE using arthematic operators.py', 'coding exercise 4.py', 'coding exercise 5.py', 'coding exercise if elif else.py', 'coding exercise on nested list.py', 'coding exercise problem 1.py', 'display.py', 'even odd.py', 'exercise on random.py', 'factorial2.py', 'fstrings example.py', 'fstrings.py', 'hello world.py', 'identity operator.py', 'if else ex 1.py', 'input_function.py', 'leap year.py', 'leet.py', 'linked list.py', 'list basics.py', 'love calculator.py', 'marks.py', 'membership operator.py', 'Mood-Based Music Recommendation System.py', 'movies.csv', 'multiple statements.py', 'name.py', 'nested elif.py', 'nested if .py', 'nested list.py', 'PAPER BATTERY.docx', 'pizza using if else elif multiple statements.py', 'random.py', 'rock paper siserrors using rand.py', 'round.py', 'set operations in python.py', 'set operations like subset disjoint.py', 'sets.py', 'smallest logical program1.py', 'students.csv', 'swapping of numbers.py', 'tuples.py', 'type of number ex.py', 'typeconversion ex 1.py', 'valid anagaram.py', '__pycache__']
+Columns in your CSV: ['song', 'artist', 'emotion']
+
+🎵 AI Mood-Based Music Recommender 🎵
+------------------------------------
+Describe your current mood: happy
+
+Detected Mood: happy energetic positive
+
+Top Recommended Songs for You:
+
+Happy - Pharrell Williams
+Can't Stop the Feeling - Justin Timberlake
+Sugar - Maroon 5
+Watermelon Sugar - Harry Styles
+Peace - Lizzo
+Hey Jude - The Beatles
+Don't Start Now - Dua Lipa
+Blinding Lights - The Weeknd
+Levitating - Dua Lipa
+Shape of You - Ed Sheeran
+>>> 
+= RESTART: C:/Users/admin/Desktop/srilekha/Mood-Based Music Recommendation System.py
+Current folder: C:\Users\admin\Desktop\srilekha
+Files in folder: ['adding digits methos 2.py', 'addition of numbers using type conversion logic.py', 'assignment 3 input function.py', 'assignment.py', 'assignment2.py', 'bill code using random.py', 'binary search.py', 'bitwise operations.py', 'BMI EXERCISE using arthematic operators.py', 'coding exercise 4.py', 'coding exercise 5.py', 'coding exercise if elif else.py', 'coding exercise on nested list.py', 'coding exercise problem 1.py', 'display.py', 'even odd.py', 'exercise on random.py', 'factorial2.py', 'fstrings example.py', 'fstrings.py', 'hello world.py', 'identity operator.py', 'if else ex 1.py', 'input_function.py', 'leap year.py', 'leet.py', 'linked list.py', 'list basics.py', 'love calculator.py', 'marks.py', 'membership operator.py', 'Mood-Based Music Recommendation System.py', 'movies.csv', 'multiple statements.py', 'name.py', 'nested elif.py', 'nested if .py', 'nested list.py', 'PAPER BATTERY.docx', 'pizza using if else elif multiple statements.py', 'random.py', 'rock paper siserrors using rand.py', 'round.py', 'set operations in python.py', 'set operations like subset disjoint.py', 'sets.py', 'smallest logical program1.py', 'students.csv', 'swapping of numbers.py', 'tuples.py', 'type of number ex.py', 'typeconversion ex 1.py', 'valid anagaram.py', '__pycache__']
+Columns in your CSV: ['song', 'artist', 'emotion']
+
+🎵 AI Mood-Based Music Recommender 🎵
+------------------------------------
+Describe your current mood: sad
+
+Detected Mood: sad emotional calm
+
+Top Recommended Songs for You:
+
+All Too Well - Taylor Swift
+Someone Like You - Adele
+drivers license - Olivia Rodrigo
+Rolling in the Deep - Adele
+Someone You Loved - Lewis Capaldi
+Someone Like You - Adele
+Happier - Marshmello
+The Archer - Taylor Swift
+I Will Always Love You - Whitney Houston
+Exist For Love - AURORA
+
+============================= RESTART: C:/Users/admin/Desktop/srilekha/Mood-Based Music Recommendation System.py ============================
+Current folder: C:\Users\admin\Desktop\srilekha
+Files in folder: ['adding digits methos 2.py', 'addition of numbers using type conversion logic.py', 'assignment 3 input function.py', 'assignment.py', 'assignment2.py', 'bill code using random.py', 'binary search.py', 'bitwise operations.py', 'BMI EXERCISE using arthematic operators.py', 'coding exercise 4.py', 'coding exercise 5.py', 'coding exercise if elif else.py', 'coding exercise on nested list.py', 'coding exercise problem 1.py', 'display.py', 'even odd.py', 'exercise on random.py', 'factorial2.py', 'fstrings example.py', 'fstrings.py', 'hello world.py', 'identity operator.py', 'if else ex 1.py', 'input_function.py', 'leap year.py', 'leet.py', 'linked list.py', 'list basics.py', 'love calculator.py', 'marks.py', 'membership operator.py', 'Mood-Based Music Recommendation System.py', 'movies.csv', 'multiple statements.py', 'name.py', 'nested elif.py', 'nested if .py', 'nested list.py', 'PAPER BATTERY.docx', 'pizza using if else elif multiple statements.py', 'random.py', 'rock paper siserrors using rand.py', 'round.py', 'set operations in python.py', 'set operations like subset disjoint.py', 'sets.py', 'smallest logical program1.py', 'students.csv', 'swapping of numbers.py', 'tuples.py', 'type of number ex.py', 'typeconversion ex 1.py', 'valid anagaram.py', '__pycache__']
+Columns in your CSV: ['song', 'artist', 'emotion']
+
+🎵 AI Mood-Based Music Recommender 🎵
+------------------------------------
+Describe your current mood: good
+
+Detected Mood: happy energetic positive
+
+Top Recommended Songs for You:
+
+Happy - Pharrell Williams
+Can't Stop the Feeling - Justin Timberlake
+Sugar - Maroon 5
+Watermelon Sugar - Harry Styles
+Peace - Lizzo
+Hey Jude - The Beatles
+Don't Start Now - Dua Lipa
+Blinding Lights - The Weeknd
+Levitating - Dua Lipa
+Shape of You - Ed Sheeran
+
+============================= RESTART: C:/Users/admin/Desktop/srilekha/Mood-Based Music Recommendation System.py ============================
+Current folder: C:\Users\admin\Desktop\srilekha
+Files in folder: ['adding digits methos 2.py', 'addition of numbers using type conversion logic.py', 'assignment 3 input function.py', 'assignment.py', 'assignment2.py', 'bill code using random.py', 'binary search.py', 'bitwise operations.py', 'BMI EXERCISE using arthematic operators.py', 'coding exercise 4.py', 'coding exercise 5.py', 'coding exercise if elif else.py', 'coding exercise on nested list.py', 'coding exercise problem 1.py', 'display.py', 'even odd.py', 'exercise on random.py', 'factorial2.py', 'fstrings example.py', 'fstrings.py', 'hello world.py', 'identity operator.py', 'if else ex 1.py', 'input_function.py', 'leap year.py', 'leet.py', 'linked list.py', 'list basics.py', 'love calculator.py', 'marks.py', 'membership operator.py', 'Mood-Based Music Recommendation System.py', 'movies.csv', 'multiple statements.py', 'name.py', 'nested elif.py', 'nested if .py', 'nested list.py', 'PAPER BATTERY.docx', 'pizza using if else elif multiple statements.py', 'random.py', 'rock paper siserrors using rand.py', 'round.py', 'set operations in python.py', 'set operations like subset disjoint.py', 'sets.py', 'smallest logical program1.py', 'students.csv', 'swapping of numbers.py', 'tuples.py', 'type of number ex.py', 'typeconversion ex 1.py', 'valid anagaram.py', '__pycache__']
+Columns in your CSV: ['song', 'artist', 'emotion']
+
+🎵 AI Mood-Based Music Recommender 🎵
+------------------------------------
+Describe your current mood: energitic
+
+Detected Mood: neutral relaxed calm
+
+Top Recommended Songs for You:
+
+Lovely Day - Bill Withers
+Circles - Post Malone
+Churchyard - AURORA
+Hello - Adele
+Stay With Me - Sam Smith
+Happier Than Ever - Billie Eilish
+Easy On Me - Adele
+Hallelujah - Leonard Cohen
+I Will Always Love You - Whitney Houston
+Exist For Love - AURORA
+
+============================= RESTART: C:/Users/admin/Desktop/srilekha/Mood-Based Music Recommendation System.py ============================
+Current folder: C:\Users\admin\Desktop\srilekha
+Files in folder: ['adding digits methos 2.py', 'addition of numbers using type conversion logic.py', 'assignment 3 input function.py', 'assignment.py', 'assignment2.py', 'bill code using random.py', 'binary search.py', 'bitwise operations.py', 'BMI EXERCISE using arthematic operators.py', 'coding exercise 4.py', 'coding exercise 5.py', 'coding exercise if elif else.py', 'coding exercise on nested list.py', 'coding exercise problem 1.py', 'display.py', 'even odd.py', 'exercise on random.py', 'factorial2.py', 'fstrings example.py', 'fstrings.py', 'hello world.py', 'identity operator.py', 'if else ex 1.py', 'input_function.py', 'leap year.py', 'leet.py', 'linked list.py', 'list basics.py', 'love calculator.py', 'marks.py', 'membership operator.py', 'Mood-Based Music Recommendation System.py', 'movies.csv', 'multiple statements.py', 'name.py', 'nested elif.py', 'nested if .py', 'nested list.py', 'PAPER BATTERY.docx', 'pizza using if else elif multiple statements.py', 'random.py', 'rock paper siserrors using rand.py', 'round.py', 'set operations in python.py', 'set operations like subset disjoint.py', 'sets.py', 'smallest logical program1.py', 'students.csv', 'swapping of numbers.py', 'tuples.py', 'type of number ex.py', 'typeconversion ex 1.py', 'valid anagaram.py', '__pycache__']
+Columns in your CSV: ['song', 'artist', 'emotion']
+
+🎵 AI Mood-Based Music Recommender 🎵
+------------------------------------
+Describe your current mood: dull
+
+Detected Mood: neutral relaxed calm
+
+Top Recommended Songs for You:
+
+Lovely Day - Bill Withers
+Circles - Post Malone
+Churchyard - AURORA
+Hello - Adele
+Stay With Me - Sam Smith
+Happier Than Ever - Billie Eilish
+Easy On Me - Adele
+Hallelujah - Leonard Cohen
+I Will Always Love You - Whitney Houston
+Exist For Love - AURORA
+
+============================= RESTART: C:/Users/admin/Desktop/srilekha/Mood-Based Music Recommendation System.py ============================
+🎵 AI Mood-Based Music Recommender 🎵
+------------------------------------
+Describe your current mood (word or sentence): happy
+
+Detected Mood(s): happy
+
+Top Recommended Songs for You:
+
+Don't Start Now - Dua Lipa
+Blinding Lights - The Weeknd
+Shape of You - Ed Sheeran
+Levitating - Dua Lipa
+Can't Stop the Feeling - Justin Timberlake
+Happy - Pharrell Williams
+As It Was - Harry Styles
+Watermelon Sugar - Harry Styles
+Sugar - Maroon 5
+Shape of You - Ed Sheeran
+
+============================= RESTART: C:/Users/admin/Desktop/srilekha/Mood-Based Music Recommendation System.py ============================
+🎵 AI Mood-Based Music Recommender 🎵
+------------------------------------
+Describe your current mood (word or sentence): energetic
+
+Detected Mood(s): happy, energetic
+
+Top Recommended Songs for You:
+
+Don't Start Now - Dua Lipa
+Blinding Lights - The Weeknd
+Shape of You - Ed Sheeran
+Levitating - Dua Lipa
+Can't Stop the Feeling - Justin Timberlake
+Happy - Pharrell Williams
+Shape of You - Ed Sheeran
+As It Was - Harry Styles
+Sugar - Maroon 5
+Watermelon Sugar - Harry Styles
+
+============================= RESTART: C:/Users/admin/Desktop/srilekha/Mood-Based Music Recommendation System.py ============================
+🎵 AI Mood-Based Music Recommender 🎵
+------------------------------------
+Describe your current mood (word or sentence): down
+
+Detected Mood(s): sad
+
+Top Recommended Songs for You:
+
+All Too Well - Taylor Swift
+Someone Like You - Adele
+drivers license - Olivia Rodrigo
+Rolling in the Deep - Adele
+Someone You Loved - Lewis Capaldi
+Someone Like You - Adele
+Happier - Marshmello
+Exile - Taylor Swift
+Lovely - Billie Eilish
+Let Her Go - Passenger
+
+============================= RESTART: C:/Users/admin/Desktop/srilekha/Mood-Based Music Recommendation System.py ============================
+🎵 AI Mood-Based Music Recommender 🎵
+------------------------------------
+Describe your current mood (word or sentence): i am sad
+
+Detected Mood(s): sad
+
+Top Recommended Songs for You:
+
+All Too Well - Taylor Swift
+Someone Like You - Adele
+drivers license - Olivia Rodrigo
+Rolling in the Deep - Adele
+Someone You Loved - Lewis Capaldi
+Someone Like You - Adele
+Happier - Marshmello
+Exile - Taylor Swift
+Lovely - Billie Eilish
+Let Her Go - Passenger
+
+============================= RESTART: C:/Users/admin/Desktop/srilekha/Mood-Based Music Recommendation System.py ============================
+🎵 AI Mood-Based Music Recommender 🎵
+------------------------------------
+Describe your current mood (word or sentence): romantic
+
+Detected Mood(s): romantic
+
+Top Recommended Songs for You:
+
+Stay - Justin Bieber
+I Will Always Love You - Whitney Houston
+Exist For Love - AURORA
+Stay With Me - Sam Smith
+Photograph - Ed Sheeran
+Willow - Taylor Swift
+Like a Star - Lizzo
+Delicate - Taylor Swift
+Style - Taylor Swift
+You Belong With Me - Taylor Swift
